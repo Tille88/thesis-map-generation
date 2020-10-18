721be83a4c2d0501eb19938bdeb0db6a0af924e2
Jonas Tillman <tillman.jonas@gmail.com>
2020-10-18 16:00:21 +0800

    Checkpoint: merging of opacity data layer

--- README.md
+++ README.md
@@ -12,25 +12,29 @@ Exportable map examples for GIS thesis on opacity-mapping
     - [x] Save canvas context (on full load event?)
         - https://openlayers.org/en/latest/examples/tile-load-events.html
 - [x] Data layer generation (Perlin noise)
-- [ ] Merge with map layer as opacity - feasibility web technology (first Canvas web-API)
-- TRY EXPORTING TO MAKE SURE OKAY
-**SUNDAY FINISHED MIN CHECKPOINT**
+- [x] Merge with map layer as opacity - feasibility web technology (first Canvas web-API)
+- [x] TRY EXPORTING TO MAKE SURE OKAY
 
-- [ ] Design data layer 
+- [ ] Design data layer = refactoring and addition of outline/smaller opacity data layer
     - [ ] color (pure RBG)
     - [ ] data value pixel mapping storage
     - [ ] outline (pure matching RBG)
+    - [ ] Seed images (data layers) for replication
 - [ ] Marker object generation (incl. range and value extraction)
     - [ ] location of marker storage - Data point as fraction x and y dimension
     - [ ] hard coded version (in canvas)
     - [ ] overlay pulsing CSS version
+**SAT OCT 24th FINISHED MIN CHECKPOINT**
+
 - [ ] Legend object types generation (using object oriented programming)
-- [ ] Seed images (data layers) for replication
 - [ ] Able to toggle marker representation
+**SUN OCT 25th FINISHED MIN CHECKPOINT**
+
 - [ ] Generate test images, and use browser API to save as large image files
     - https://www.tutorialspoint.com/HTML5-Canvas-to-PNG-File
     - https://stackoverflow.com/questions/923885/capture-html-canvas-as-gif-jpg-png-pdf
 - [ ] Downsample or generate different sizes programmatically
+- [ ] Export all layers separately for one to be shown in intro-page
 
 No automated testing needed
 
--- src/noisedata.js
+++ src/noisedata.js
@@ -3,26 +3,73 @@ import cfg from './cfg';
 import {getCanvasContext} from './utils';
 
 export const CreateNoiseDataLayer = function({} = {}){
+    let alphaStorage = null;
     return {
         init: function(){
             let ctx = getCanvasContext();
             let width = ctx.canvas.width;
             let height = ctx.canvas.height;
             // p5.noiseSeed(123);
-            //   var imageData = ctx.getImageData(0, 0, width, height);
-            // ALT
-            var imageData = ctx.createImageData(width,height); 
+            var imageData = ctx.getImageData(0, 0, width, height);
+            alphaStorage = new Array(imageData.data.length);
             for (let i = 0; i < imageData.data.length; i += 4) {
                 let x = (i/4) % width;
                 let y = ((i/4) - x) / width;
-                imageData.data[i + 0] = 255;  // R value
-                imageData.data[i + 1] = 0;    // G value
-                imageData.data[i + 2] = 0;  // B value
-                imageData.data[i + 3] = Math.floor(p5.noise(0.005*x, 0.005*y) * 255);
+                let noiseVal = p5.noise(0.005*x, 0.005*y);
+                // let noiseVal = p5.noise(0.1*x, 0.1*y);
+                let noiseAlpha = Math.floor(noiseVal * 255);
+                if(x<500&&y<500){ // TEMP TODO=REMOVE AND HAVE NICER SECTION...
+                    // https://ciechanow.ski/alpha-compositing/
+                    // https://www.w3.org/TR/compositing-1/#simplealphacompositing (background=dest=opaque)
+                    // R value
+                    imageData.data[i + 0] = noiseVal*255 + imageData.data[i + 0]*(1-noiseVal);  
+                    // G value
+                    imageData.data[i + 1] = noiseVal*0 + imageData.data[i + 2]*(1-noiseVal);  
+                    // B value  
+                    imageData.data[i + 2] = noiseVal*0 + imageData.data[i + 2]*(1-noiseVal);  
+                    // Alpha value
+                    // 1 - (1 - Ea) * (1 - Ca) - TODO: needs inclusion or simply opaque=255?
+                    imageData.data[i + 3] = (1 - (1-noiseVal) * (1 - imageData.data[i + 3]/255))*255;
+                }
+                alphaStorage[Math.ceil(i/4)] = noiseVal;
               }
-              
               // Draw image data to the canvas
               ctx.putImageData(imageData, 0, 0);
         }
     }
 }
+
+
+
+// BACKUP -> NEEDS TO BE EXPORTED SEPARATELY FOR "SEPARETED IMAGE LAYERS FOR START PAGE"
+// export const CreateNoiseDataLayer = function({} = {}){
+//     let alphaStorage = null;
+//     return {
+//         init: function(){
+//             // console.log(this, this.alphaStorage);
+//             // debugger;
+//             let ctx = getCanvasContext();
+//             let width = ctx.canvas.width;
+//             let height = ctx.canvas.height;
+//             // p5.noiseSeed(123);
+//             // var imageData = ctx.getImageData(0, 0, width, height);
+//             // ALT
+//             var imageData = ctx.createImageData(width,height);
+//             alphaStorage = new Array(imageData.data.length);
+//             for (let i = 0; i < imageData.data.length; i += 4) {
+//                 let x = (i/4) % width;
+//                 let y = ((i/4) - x) / width;
+//                 imageData.data[i + 0] = 255;  // R value
+//                 imageData.data[i + 1] = 0;    // G value
+//                 imageData.data[i + 2] = 0;  // B value
+//                 let noiseVal = p5.noise(0.005*x, 0.005*y);
+//                 imageData.data[i + 3] = Math.floor(noiseVal * 255);
+//                 alphaStorage[Math.ceil(i/4)] = noiseVal;
+//               }
+//               // Draw image data to the canvas
+//               ctx.putImageData(imageData, 0, 0);
+//         }
+//     }
+// }
+
+
